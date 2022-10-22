e31c408aa846aba9c409e930fc0abdf001733f63
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-22 18:21:11 +0400

    MVP

---
+++ src/lib/graphql.ts
@@ -0,0 +1,23 @@
+const SERVER_URL = "https://vwkd-kita-conj-server.deno.dev/api";
+
+export async function gql_op({ query, variables, operationName, returnKey }) {
+  const headers = { "Content-Type": "application/json" };
+
+  const body = JSON.stringify({ query, variables, operationName });
+
+  const res = await fetch(SERVER_URL, {
+    method: "POST",
+    headers,
+    body,
+  });
+
+  const response = await res.json();
+
+  if (response.errors) {
+    throw new Error("gql_op errors", response.errors);
+  }
+  
+  const data = response.data[returnKey];
+
+  return data;
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,81 @@
 import { Head } from "$fresh/runtime.ts";
+import { getEntry } from "../lib/operations.ts";
 
-export default function Home() {
+export const handler = {
+  async GET(req, ctx) {
+    const url = new URL(req.url);
+    const id = url.searchParams.get("id");
+
+    if (id === null) {
+      return new Response("Redirecting to /", {
+        status: 307,
+        headers: { location: "/" },
+      });
+    } else {
+      // TODO: error handling
+      const entry = await getEntry(id);
+
+      return ctx.render({ ...entry });
+    }
+  },
+};
+
+export default function Home({ data }) {
   return (
     <div>
       <Head>
         <link rel="stylesheet" href="/reset.css" />
       </Head>
+      <main class="flex-1">
+        { data.value
+          ? <Entry {...data} />
+          : <p>No entry found.</p>
+        }
+      </main>
     </div>
   );
+}
+
+function Entry({ id, value }) {
+  return (
+    <>
+      <Table {...value} />    
+    </>
+  );
+}
+
+function Table({ PRS }) {
+  return (
+    <>
+      <Screeve {...PRS} />    
+    </>
+  );
+}
+
+function Screeve({ S1 }) {
+  return (
+    <>
+      <Form {...S1} />    
+    </>
+  );
+}
+
+function Form({ person1, root, person2 }) {
+  return (
+    <>
+      <Component {...person1} />
+      <Component {...root} />
+      <Component {...person2} />
+    </>
+  );
+}
+
+function Component({ key, value }) {
+  if (value) {
+    return (
+      <span>{value}</span>
+    );
+  } else {
+    return null;
+  }
 }
