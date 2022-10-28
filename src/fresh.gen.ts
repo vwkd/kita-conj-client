18c772a18ec5a5f974708a648bb116fc873d971e
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-28 17:47:36 +0400

    routes: move to entry

--- src/fresh.gen.ts
+++ src/fresh.gen.ts
@@ -4,10 +4,12 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/index.tsx";
+import * as $1 from "./routes/entry/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/index.tsx": $0,
+    "./routes/entry/index.tsx": $1,
   },
   islands: {},
   baseUrl: import.meta.url,
