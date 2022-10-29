ed2a854e3c34e7c38982b39dede2d0483b914ead
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-29 08:00:47 +0400

    entry: use 404 if not found

---
+++ src/routes/_404.tsx
@@ -0,0 +1,18 @@
+import Layout from "../components/Layout.tsx";
+
+const META = name => ({
+  title: `${name} Not Found`,
+  description: `${name} not found`,
+});
+
+export default function Page({ data }) {
+  const name = data?.name ?? "Page";
+  return (
+    <Layout {...META(name)}>
+      <main class="flex-1 flex-col gap-3">
+        <h1 class="text-xl font-bold">Not Found</h1>
+        <p>{`${name} not found.`}</p>
+      </main>
+    </Layout>
+  );
+}
