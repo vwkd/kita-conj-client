1b68a12f39e3e6507771dbe02b9b54c5b94735a6
vwkd <33468089+vwkd@users.noreply.github.com>
2022-10-31 12:06:52 +0400

    use grid

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -46,7 +46,7 @@ function TableOrException({ value }) {
 
 function Table({ SRS1, SRS2, SRS3 }) {
   return (
-    <div class="flex-col gap-2">
+    <div class="flex-col gap-3">
       <SRS1OrException value={SRS1} />
       <SRS2OrException value={SRS2} />
       <SRS3OrException value={SRS3} />
@@ -56,7 +56,7 @@ function Table({ SRS1, SRS2, SRS3 }) {
 
 function SRS1OrException({ value }) {
   return (
-    <div class="flex-col gap-2">
+    <div class="flex-col gap-1">
       <h2 class="text-lg font-semibold text-slate-500">Series 1</h2>
       { value.note
         ? <Exception {...value} />
@@ -68,7 +68,7 @@ function SRS1OrException({ value }) {
 
 function SRS1_({ GRP1, GRP2 }) {
   return (
-    <div class="flex-col gap-1">
+    <div class="flex-col gap-3">
       <GRP1OrException value={GRP1} />
       <GRP2OrException value={GRP2} />
     </div>
@@ -89,7 +89,7 @@ function GRP1OrException({ value }) {
 
 function GRP1_({ PRS, IMPF, PRSSUBJ }) {
   return (
-    <div class="flex-col gap-1">
+    <div class="flex-col gap-3">
       <ScreeveOrException Screeve={Screeve} value={PRS} label="Present" />
       <ScreeveOrException value={IMPF} label="Imperfect" />
       <ScreeveOrException value={PRSSUBJ} label="Present Subjunctive" />
@@ -111,7 +111,7 @@ function GRP2OrException({ value }) {
 
 function GRP2_({ FUT, COND, FUTSUBJ }) {
   return (
-    <div class="flex-col gap-1">
+    <div class="flex-col gap-3">
       <ScreeveOrException value={FUT} label="Future" />
       <ScreeveOrException value={COND} label="Conditional" />
       <ScreeveOrException value={FUTSUBJ} label="Future Subjunctive" />
@@ -121,7 +121,7 @@ function GRP2_({ FUT, COND, FUTSUBJ }) {
 
 function SRS2OrException({ value }) {
   return (
-    <div class="flex-col gap-2">
+    <div class="flex-col gap-1">
       <h2 class="text-lg font-semibold text-slate-500">Series 2</h2>
       { value.note
         ? <Exception {...value} />
@@ -133,7 +133,7 @@ function SRS2OrException({ value }) {
 
 function SRS2_({ AOR, AORIMPF, OPT, OPTIMPF }) {
   return (
-    <div class="flex-col gap-1">
+    <div class="flex-col gap-3">
       <ScreeveOrException value={AOR} label="Aorist" />
       <ScreeveOrException value={AORIMPF} label="Aorist Imperfective" />
       <ScreeveOrException value={OPT} label="Optative" />
@@ -144,7 +144,7 @@ function SRS2_({ AOR, AORIMPF, OPT, OPTIMPF }) {
 
 function SRS3OrException({ value }) {
   return (
-    <div class="flex-col gap-2">
+    <div class="flex-col gap-1">
       <h2 class="text-lg font-semibold text-slate-500">Series 3</h2>
       { value.note
         ? <Exception {...value} />
@@ -156,7 +156,7 @@ function SRS3OrException({ value }) {
 
 function SRS3_({ PERF, PERFIMPF, PLUPERF, PLUPERFIMPF, PERFSUBJ, PERFSUBJIMPF }) {
   return (
-    <div class="flex-col gap-1">
+    <div class="flex-col gap-3">
       <ScreeveOrException value={PERF} label="Perfect" />
       <ScreeveOrException value={PERFIMPF} label="Perfect Imperfective" />
       <ScreeveOrException value={PLUPERF} label="Pluperfect" />
@@ -181,8 +181,8 @@ function ScreeveOrException({ value, label }) {
 
 function Screeve({ S1, S2, S3, P1, P2, P3 }) {
   return (
-    <table class="flex-col">
-      <tbody class="flex-col">
+    <table class="grid grid-rows-6 grid-cols-8 gap-x-3 justify-start max-w-max">
+      <tbody class="row-span-full col-span-full grid" style="grid-template-rows: subgrid; grid-template-columns: subgrid;">
         <FormOrException value={S1} label="S1" />
         <FormOrException value={S2} label="S2" />
         <FormOrException value={S3} label="S3" />
@@ -196,7 +196,7 @@ function Screeve({ S1, S2, S3, P1, P2, P3 }) {
 
 function FormOrException({ value, label }) {
   return (
-    <tr class="flex-1 gap-3 items-baseline">
+    <tr class="col-span-full grid items-baseline" style="grid-template-columns: subgrid;">
       <th class="text-sm font-semibold text-slate-500">{label}</th>
       { value.note
         ? <Exception {...value} />
@@ -208,7 +208,7 @@ function FormOrException({ value, label }) {
 
 function Form({ preverb, person1, version, root, thema, modus, person2 }) {
   return (
-    <td>
+    <td class="col-start-2 col-end-9 grid gap-x-1 items-baseline" style="grid-template-columns: subgrid;">
       <ComponentOrException value={preverb} label="Preverb" />
       <ComponentOrException value={person1} label="Person1" />
       <ComponentOrException value={version} label="Version" />
